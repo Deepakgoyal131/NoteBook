c0748af462098acc089dc70d277662c9f1726ca7
Deepakgoyal131 <goyaldeepak7743@gmail.com>
2025-09-03 17:15:03 +0530

    change

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href='#'>iNoteBook</a>
+                <h1 className="navbar-brand" href='#'>iNoteBook</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
