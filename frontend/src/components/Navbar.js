e5ee0ce0b7a89b7da4002419ea84b897912e52b4
Deepakgoyal131 <goyaldeepak7743@gmail.com>
2025-09-03 17:13:58 +0530

    update performance

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,10 @@
-import React, { useEffect } from 'react'
-import {Link, useLocation} from 'react-router-dom';
+import React from 'react'
+import {Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     let navigate = useNavigate();
-    let location = useLocation();
-    useEffect(()=>{
-        console.log(location.pathname);
-    },[location])
+    
 
     const handleLogout = ()=>{
         localStorage.removeItem('token');
@@ -17,15 +14,13 @@ const Navbar = () => {
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">iNoteBook</Link>
+                <a className="navbar-brand" href='#'>iNoteBook</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/" ? "active": ""}`}aria-current="page" to="/">Home</Link>
-                        </li>
+                        
                     </ul>
                     {!localStorage.getItem('token')?<form className="d-flex">
                         <Link className="btn btn-outline-primary mx-2" role="button" to='/login'>Login</Link>
--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,9 +2,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  
+
   const host = process.env.REACT_APP_API_HOST;
- 
+
   const notesInitial = [];
 
   const [notes, setNotes] = useState(notesInitial)
@@ -12,93 +12,113 @@ const NoteState = (props) => {
   //Get all Note     
   const getNotes = async () => {
     // API call
-    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json", 
-        "auth-token":  localStorage.getItem('token')
-      }  
-    });
-    const json = await response.json();
-    // console.log(json)
-    setNotes(json)
-    
+    try {
+      const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        }
+      });
+      const json = await response.json();
+
+      setNotes(json)
+    } catch (error) {
+      alert("try and login after some time unable to perform action")
+    }
+
+
   }
 
   //Add a Note
   const addNote = async (title, description, tag) => {
     // API call
-    
-    const response = await fetch(`${host}/api/notes/addnote`, {
-      method: "POST", 
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": localStorage.getItem('token')
-      },
-      body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
-    });
-    const json = await response.json();
-    console.log(json);
 
-    console.log("Adding a new note")
-    const note = {
-      "_id": json._id,
-      "user": json.user,
-      "title": json.title,
-      "description": json.description,
-      "tag": json.tag,
-      "name": json.name,
-      "__v": json._v
+    try {
+      const response = await fetch(`${host}/api/notes/addnote`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        },
+        body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
+      });
+      const json = await response.json();
+      console.log(json);
+
+      console.log("Adding a new note")
+      const note = {
+        "_id": json._id,
+        "user": json.user,
+        "title": json.title,
+        "description": json.description,
+        "tag": json.tag,
+        "name": json.name,
+        "__v": json._v
+      }
+      setNotes(notes.concat(note));
+    } catch (error) {
+      alert("try and login after some time! unable to perform action")
     }
-    setNotes(notes.concat(note));
+
   }
 
   //Delete a Note
   const deleteNote = async (id) => {
     // API CALL
-    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
-      method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": localStorage.getItem('token')
-      },
-      
-    });
-    const json = await response.json();
-    console.log(json);
-  
-    console.log("Note delete " + id)
-    const newNotes = notes.filter((note) => { return note._id !== id })
-    setNotes(newNotes);
+    try {
+      const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+        method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        },
+
+      });
+      const json = await response.json();
+      console.log(json);
+
+      console.log("Note delete " + id)
+      const newNotes = notes.filter((note) => { return note._id !== id })
+      setNotes(newNotes);
+    } catch (error) {
+      alert("try and login after some time unable to perform action")
+    }
+
   }
 
   //Edit a Note
   const editNote = async (id, title, description, tag) => {
     //API Call
-    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "PUT", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": localStorage.getItem('token')
-      },
-      body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
-    });
-    const json = await response.json();
-    console.log(json)
+    try {
+      const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+        method: "PUT", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        },
+        body: JSON.stringify({ title, description, tag }), // body data type must match "Content-Type" header
+      });
+      const json = await response.json();
+      console.log(json)
 
-    let newNotes = JSON.parse(JSON.stringify(notes))
-    // logic to edit in client
-    for (let i = 0; i < notes.length; i++) {
-      const element = newNotes[i];
-      if (element._id === id) {
-        newNotes[i].title = title;
-        newNotes[i].description = description;
-        newNotes[i].tag = tag;
-        break;
+      let newNotes = JSON.parse(JSON.stringify(notes))
+      // logic to edit in client
+      for (let i = 0; i < notes.length; i++) {
+        const element = newNotes[i];
+        if (element._id === id) {
+          newNotes[i].title = title;
+          newNotes[i].description = description;
+          newNotes[i].tag = tag;
+          break;
+        }
+
       }
-      
+      setNotes(newNotes);
+    } catch (error) {
+      alert("try and login after some time unable to perform action")
     }
-    setNotes(newNotes);
+
   }
 
   return (
